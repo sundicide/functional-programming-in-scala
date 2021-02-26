c5f8b6a0071f8b450370b66c9debddd97640d5b2
sundicide <kyusuen@gmail.com>
2021-02-27 07:28:21 +0900

    add Option

---
+++ src/main/scala/fpinscala/datastructures/Option.scala
@@ -0,0 +1,14 @@
+package fpinscala.datastructures
+
+sealed trait Option[+A]
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+
+trait Options[+A] {
+  def map[B](f: A => B): Option[B]
+  def flatMap[B](f: A => Option[B]): Option[B]
+  def getOrElse[B >: A](default: => B): B
+  def orElse[B >: A](ob: => Option[B]): Option[B]
+  def filter(f: A => Boolean): Option[A]
+}
