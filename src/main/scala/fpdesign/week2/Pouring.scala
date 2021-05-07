827ad23339963f599ff0425f10689408dd54bd66
sundicide <kyusuen@gmail.com>
2021-05-07 20:58:42 +0900

    add week2 codes

---
+++ src/main/scala/fpdesign/week2/Pouring.scala
@@ -0,0 +1,81 @@
+package fpdesign.week2
+
+class Pouring(capicity: Vector[Int]) {
+
+ // States
+ type State = Vector[Int]
+ val initialState = capicity map (x => 0)
+
+ // Moves
+ trait Move {
+   def change(state: State): State
+ }
+ case class Empty(glass: Int) extends Move {
+   def change(state: State) = state updated (glass, 0)
+ }
+ case class Fill(glass: Int) extends Move {
+   def change(state: State) = state updated (glass, capicity(glass))
+ }
+ case class Pour(from: Int, to: Int) extends Move {
+   def change(state: State) = {
+     val amount = state(from) min (capicity(to) - state(to))
+     state updated (from, state(from) - amount) updated (to, state(to) + amount)
+   }
+ }
+
+ val glasses = 0 until capicity.length
+
+  // 가능한 모든 경우의 수
+  // input이 new Pouring(Vector(4, 7))일 경우
+  // Empty(0), Empty(1), Fill(0), Fill(1), Pour(0, 1), Pour(1, 0)
+ val moves =
+    (for (g <- glasses) yield Empty(g)) ++
+    (for (g <- glasses) yield Fill(g)) ++
+    (for (from <- glasses; to <- glasses if from != to) yield Pour(from, to))
+
+  // Paths
+   class Path(history: List[Move], val endState: State) {
+//    def endState: State = (history foldRight initialState) (_ change _)
+
+//    def endState: State = trackState(history)
+//    private def trackState(xs: List[Move]): State = xs match {
+//      case Nil => initialState
+//      case move :: xs1 => move change trackState(xs1)
+//    }
+
+    def extend(move: Move) = new Path(move :: history, move change endState)
+    override def toString = (history.reverse mkString " ") + " --> " + endState
+  }
+
+   val initialPath = new Path(Nil, initialState)
+
+   def from(paths: Set[Path], explored: Set[State]): Stream[Set[Path]] =
+     if (paths.isEmpty) Stream.empty
+     else {
+       val more = for {
+         path <- paths
+         next <- moves map path.extend
+         if !(explored contains next.endState)
+       } yield next
+       paths #:: from(more, explored ++ (more map (_.endState)))
+     }
+
+   val pathSets = from(Set(initialPath), Set(initialState))
+
+  def solutions(target: Int): Stream[Path] =
+    for {
+      pathSet <- pathSets
+      path <- pathSet
+      if path.endState contains target
+    } yield path
+}
+object test {
+  def main(args: Array[String]): Unit = {
+    val problem = new Pouring(Vector(4, 9, 19))
+    println(problem.moves)
+
+    println(problem.pathSets.take(3).toList)
+
+    println(problem.solutions(6))
+  }
+}
--- src/main/scala/fpdesign/week2/test.sc
+++ src/main/scala/fpdesign/week2/test.sc
@@ -1,3 +1,5 @@
+import fpdesign.week2.primes.from
+
 def streamRange(lo: Int, hi: Int): Stream[Int] = {
   print(lo + " ")
   if (lo >= hi) Stream.empty
@@ -5,3 +7,47 @@ def streamRange(lo: Int, hi: Int): Stream[Int] = {
 }
 
 streamRange(1, 10).tail(1)
+
+
+def expr = {
+  val x = { print("x"); 1 }
+  lazy val y = { print("y"); 2 }
+  def z = { print("z"); 3 }
+
+  z + y + x + z + y + x
+}
+expr
+
+def from(n: Int): Stream[Int] = n #:: from(n+1)
+
+val nats = from(0)
+
+val m4s = nats map (_ * 4)
+
+(m4s take 100).toList
+
+
+// Sieve of Eratosthenes
+// ancient technique to calculate prime number
+// 1. 2부터 시작
+// 2. 소수가 있으면 그의 배수가 되는 항목들은 제외한다.
+// 3. 다음 숫자로 넘어간다.
+// 4. 반복
+def sieve(s: Stream[Int]): Stream[Int] =
+  s.head #:: sieve(s.tail filter (_ % s.head != 0))
+
+val primes = sieve(from(2))
+primes.take(100).toList
+
+
+def sqrtStream(x: Double): Stream[Double] = {
+  def imporve(guess: Double) = (guess + x / guess) / 2
+  lazy val guesses: Stream[Double] = 1 #:: (guesses map imporve)
+  guesses
+}
+sqrtStream(4).take(10).toList
+
+def isGoodEnough(guess: Double, x: Double) =
+  math.abs((guess * guess - x) / x) < 0.0001
+
+sqrtStream(4).filter(isGoodEnough(_, 4)).take(10).toList
